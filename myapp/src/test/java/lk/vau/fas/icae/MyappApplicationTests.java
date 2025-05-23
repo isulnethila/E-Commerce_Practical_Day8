c6a78d0e5698c1f46588bffe8fcd7c7bf321c3c7
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-05-23 13:45:53 +0530

    create project

---
+++ myapp/src/test/java/lk/vau/fas/icae/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.vau.fas.icae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
